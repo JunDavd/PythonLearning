36a2d2a9a075014f467d396bdc48890f1a8206f2
Juan Ruiz <jdruiz008@gmail.com>
2025-02-16 17:31:52 -0500

    update: added gitignore file and finished is_valid_name_for_god() function

--- ejercicios_busqueda.py
+++ ejercicios_busqueda.py
@@ -35,7 +35,7 @@ def letters_limit(string,limit=9):
   else:
       between_string_limit = False
   return between_string_limit
-print(letters_limit("aolaautfu"))
+#print(letters_limit("aolaautfu"))
 
 def only_lowercase(string):
     """
@@ -44,31 +44,61 @@ def only_lowercase(string):
     caracteres estan en minusculas
     """
     result_only_lowercase = False
+    if string == '' or string == ' ':
+        return False
     if string == string.lower():
         result_only_lowercase = True
     return result_only_lowercase
 
-print(only_lowercase('Rgtzhdjahab'))
+#print(only_lowercase('gtzhdjahab'))
 
 def consonants_limit(string,limit = 3):
     """
-    :param string:
-    :param limit:
-    :return:
+    :param string: recibe una cadena de caracteres
+    :param limit: la cadena de caracteres no debe tener más de 3 consonantes
+    repetidas
+    :return: devuelve False si hay más de 3 consonantes seguidas
     """
-    result_consonants_limit = True
+    result_consonants_within_limit = True
     consonants_occ = 0
     consonants = "bcdfghjklmnñpqrstvwxyz"
     for string_element in string:
         if string_element in consonants:
             consonants_occ += 1
             if consonants_occ > limit:
-                result_consonants_limit = False
+                result_consonants_within_limit = False
                 break
         else:
             consonants_occ = 0
-    return result_consonants_limit
+    return result_consonants_within_limit
 
-#print(consonants_limit('aeiouty'))
+def is_valid_name_for_god(string):
+    """
+    :param string: cadena de caracteres que debe ser verificada con una serie de predicados
+    :return: devuelve un booleano True o False, devuelve True si la cadena cumple con las
+    siguientes condiciones:
+    1. la cadena tiene que tener 9 letras, ni más ni menos.
+    2. no puede contener números, sólo letras del alfabeto español "abcdefghijklmnñopqrstuvwxyz"
+    3. las letras tienen que estar en minúsculas (las mayúsculas no son aceptables)
+    4. la cadena no puede contener más de 3 consonantes seguidas. Puede no tener ninguna, una, dos o tres, pero nunca 4 o más.
+    """
+    result_is_valid_name = False
+    if string != str(string):
+        return False
+    if only_lowercase(string):
+        if letters_limit(string):
+            if consonants_limit(string):
+                result_is_valid_name = True
+    return result_is_valid_name
+
+print(is_valid_name_for_god('aaaaa'))
+print(is_valid_name_for_god('o22iUijpq'))
+print(is_valid_name_for_god('auertctoa'))
+print(is_valid_name_for_god('aaaaaaaaa'))
+print(is_valid_name_for_god('azaxaaktp'))
+print(is_valid_name_for_god('aoieyasul'))
+print(is_valid_name_for_god(8))
+
+
 
 
